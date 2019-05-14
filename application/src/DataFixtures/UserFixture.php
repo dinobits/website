94bf6e98d4b35348344d1ce4bbfaed50fc226b9e
Dinozor <dinocrys@gmail.com>
2019-05-15 01:05:23 +0300

    Add mysql docker container Add doctrine migration for user db Add admin user fixture Add authentication (security controller with additional data)

---
+++ application/src/DataFixtures/UserFixture.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class UserFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('admin@admin.com');
+        $user->setPassword('$argon2i$v=19$m=1024,t=2,p=2$T3J2cUczdW5UajBkZm9LOA$lnNXaZIpb8HE57AVYZ5YzpuDwSUeyOBFK5FlzkBBg8U');
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
